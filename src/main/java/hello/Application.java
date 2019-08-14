37bab18f142f888c6e133adaecfd18dfc55ab32e
yeob <okayhan89@gmail.com>
2019-08-14 15:38:56 +0900

    test

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,31 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World.";
+	}
+
+	@RequestMapping("/sample-gs-spring-boot-docker")
+	public String home2() {
+		return "Hello ucking there.";
+	}
+
+	@RequestMapping("/sample")
+	public String home3() {
+		return "Hello ru sober?";
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
